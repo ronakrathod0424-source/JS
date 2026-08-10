86038617d7043ccd9e4ef176e056dd768e0d960b
js <ronakrathod0424@gmail.com>
2026-08-11 03:02:40 +0530

    how to convet value into Number,Boolean,String

---
+++ 03_conversionOperation.js
@@ -0,0 +1,28 @@
+ // normal excode
+
+// let a = 33                  
+// console.log(typeof (a));    // Output :Number
+// console.log(a);             // Output :33
+
+
+// how to convet value into Number,Boolean,String........
+// let a = "10"
+// console.log(typeof(a));    // Output :String
+//  let b = Number(a);         
+// console.log(typeof(b));    // Output :Number
+
+// ****************** if **************************
+let a = "10abc"
+console.log(typeof(a));       // Output :String
+
+ let b = Number(a);         
+console.log(typeof(b));       // Output :number   But
+console.log(b);               // Output :NaN
+
+
+
+
+
+
+ 
+
